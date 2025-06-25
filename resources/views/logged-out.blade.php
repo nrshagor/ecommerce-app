aad24a194217de83a9b0d887526ad20516ba0822
nrshagor <noorerabbishagor@gmail.com>
2025-06-25 11:26:43 +0600

    feat: Simplify logout process by using a hidden window for Foodpanda SSO logout

--- resources/views/logged-out.blade.php
+++ resources/views/logged-out.blade.php
@@ -6,18 +6,13 @@
 <body>
     <h2>Logging you out from all systems. Please wait...</h2>
 
-   <form id="ssoLogoutForm" action="http://foodpanda.local:8001/sso-logout" method="GET" target="logoutTarget">
-</form>
-
-<iframe name="logoutTarget" style="display:none;"></iframe>
-
-<script>
-    document.getElementById('ssoLogoutForm').submit();
-
-    setTimeout(function () {
-        window.location.href = '/';
-    }, 2000);
-</script>
-
+    <script>
+        // Open logout page in a hidden window 
+        const win = window.open('http://foodpanda.local:8001/sso-logout','_blank', 'width=1,height=1,left=-1000,top=-1000');
+        setTimeout(() => {
+            win && win.close();
+            window.location.href = '/';
+        }, 100);
+    </script>
 </body>
 </html>
